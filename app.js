4e5d4171e93c3aef6788b7523671c4c027cf7a3c
Preeti Uppuluri <puppuluri@godaddy.com>
2022-07-19 10:24:37 -0400

    working /test command

---
+++ app.js
@@ -0,0 +1,26 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true, // enable the following to use socket mode
+    appToken: process.env.APP_TOKEN
+});
+
+app.command("/test", async ({ command, ack, say }) => {
+    try {
+        await ack();
+        say("Test is a success!");
+    } catch (error) {
+        console.log("err")
+        console.error(error);
+    }
+});
+
+(async () => {
+    const port = 3000
+    // Start your app
+    await app.start(process.env.PORT || port);
+    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
