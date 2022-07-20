151a6126fd1eb2a33be00efad35fafceaf57c6c4
Basheer Alkhalil <balkhalil@godaddy.com>
2022-07-20 02:03:53 -0700

    rebasev2

--- match.js
+++ match.js
@@ -8,6 +8,7 @@ module.exports.findMatch = async function(json_input, bool){
 
     var currentDB = await interestController.fetchInterests(current_interests);
     if (bool){
+        console.log('need save data'+ current_interests);
         console.log('need save data'+ current_user);
         await userController.createUpdateUser(json_input);
     }
@@ -37,7 +38,7 @@ module.exports.findMatch = async function(json_input, bool){
         }
     }
 
-
+//checkgit
     // var similar_users = [];
     var most_similar_user = "";
     var most_similar_user_cnt = 0;
